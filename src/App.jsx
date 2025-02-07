32076becff347759305674c27a1994a7d7ba77b9
Aditya Kumar <95904079+Aditya-Ajay@users.noreply.github.com>
2025-02-07 12:17:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
 
     try {
       const response = await fetch('http://localhost:5000/api/chat/astro', {
+        mode :'no-cors',
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
